6e0214ccefc3ed54c66d9b8418bcb81aeaee027d
SATvanegas <svanegasv@unal.edu.co>
2025-01-23 18:38:08 -0500

    Create and define ProductClass

---
+++ src/main/java/com/prebel/prototipo/webapp/models/Product.java
@@ -0,0 +1,12 @@
+package com.prebel.prototipo.webapp.models;
+
+public class Product {
+
+    private String reference;
+    private int batch;
+    private String packagingType; // to define if it remains as a java enum
+    private String packagingMaterial; // to define if it remains as a java enum
+    private String containerColor; // to define if it remains as a java enum
+    private String lidMaterial; // to define if it remains as a java enum
+    private String lidColor; // to define if it remains as a java enum
+}

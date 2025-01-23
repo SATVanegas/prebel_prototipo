f9acc308183bea9bbf7d1b6bc8806660d2f6ae0a
SATvanegas <svanegasv@unal.edu.co>
2025-01-23 12:40:00 -0500

    Initial commit

---
+++ src/main/java/com/prebel/prototipo/webapp/PrebelPrototipoApplication.java
@@ -0,0 +1,13 @@
+package com.prebel.prototipo.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrebelPrototipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrebelPrototipoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prebel/prototipo/webapp/PrebelPrototipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prebel.prototipo.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrebelPrototipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

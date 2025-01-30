c964d80d061d443105013374c06653c384883a51
SATvanegas <svanegasv@unal.edu.co>
2025-01-29 21:10:46 -0500

    Update User.java

--- src/main/java/com/prebel/prototipo/webapp/models/User.java
+++ src/main/java/com/prebel/prototipo/webapp/models/User.java
@@ -2,9 +2,6 @@ package com.prebel.prototipo.webapp.models;
 
 import jakarta.persistence.*;
 
-import java.util.HashMap;
-import java.util.List;
-
 @Entity
 @Table(name = "users")
 public class User {

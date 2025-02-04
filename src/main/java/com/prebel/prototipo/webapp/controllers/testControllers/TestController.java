0a34030b504f934c0f36a8c58168a2f04816a194
SATvanegas <svanegasv@unal.edu.co>
2025-02-03 20:40:40 -0500

    Update TestController.java

--- src/main/java/com/prebel/prototipo/webapp/controllers/testControllers/TestController.java
+++ src/main/java/com/prebel/prototipo/webapp/controllers/testControllers/TestController.java
@@ -2,7 +2,7 @@ package com.prebel.prototipo.webapp.controllers.testControllers;
 
 
 import com.prebel.prototipo.webapp.models.Test;
-import com.prebel.prototipo.webapp.repositories.testRepositories.TestRepository;
+import com.prebel.prototipo.webapp.repositories.testRepositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +13,41 @@ import java.util.Optional;
 @RequestMapping("/api/test")
 public class TestController {
 
-    @Autowired
-    private TestRepository testRepository;
+
+    private final TestRepository testRepository;
+    private final AppearanceRepository appearanceRepository;
+    private final ColorRepository colorRepository;
+    private final FungiYeastCountRepository fungiYeastCountRepository;
+    private final OdorRepository odorRepository;
+    private final PathogensRepository pathogensRepository;
+    private final pHRepository phRepository;
+    private final SpecificGravityRepository specificGravityRepository;
+    private final StorageRepository storageRepository;
+    private final TemperatureRepository temperatureRepository;
+    private final TotalBacteriaCountRepository totalBacteriaCountRepository;
+    private final ViscosityRepository viscosityRepository;
+
+
+    public TestController(TestRepository testRepository, AppearanceRepository appearanceRepository,
+                          ColorRepository colorRepository, FungiYeastCountRepository fungiYeastCountRepository,
+                          OdorRepository odorRepository, PathogensRepository pathogensRepository,
+                          pHRepository phRepository, SpecificGravityRepository specificGravityRepository,
+                          StorageRepository storageRepository, TemperatureRepository temperatureRepository,
+                          TotalBacteriaCountRepository totalBacteriaCountRepository,
+                          ViscosityRepository viscosityRepository) {
+        this.testRepository = testRepository;
+        this.appearanceRepository = appearanceRepository;
+        this.colorRepository = colorRepository;
+        this.fungiYeastCountRepository = fungiYeastCountRepository;
+        this.odorRepository = odorRepository;
+        this.pathogensRepository = pathogensRepository;
+        this.phRepository = phRepository;
+        this.specificGravityRepository = specificGravityRepository;
+        this.storageRepository = storageRepository;
+        this.temperatureRepository = temperatureRepository;
+        this.totalBacteriaCountRepository = totalBacteriaCountRepository;
+        this.viscosityRepository = viscosityRepository;
+    }
 
     // Buscar un Test por ID
     @GetMapping("/{id}")
@@ -25,5 +58,23 @@ public class TestController {
     }
 
     // Crear un nuevo Test
-    
+// Crear un nuevo Test
+    @PostMapping
+    public ResponseEntity<Test> createTest(@RequestBody Test test) {
+        test.setAppearance(appearanceRepository.findById(test.getAppearance().getId()).orElse(null));
+        test.setColor(colorRepository.findById(test.getColor().getId()).orElse(null));
+        test.setFungiYeastCount(fungiYeastCountRepository.findById(test.getFungiYeastCount().getId()).orElse(null));
+        test.setOdor(odorRepository.findById(test.getOdor().getId()).orElse(null));
+        test.setPathogens(pathogensRepository.findById(test.getPathogens().getId()).orElse(null));
+        test.setPh(phRepository.findById(test.getPh().getId()).orElse(null));
+        test.setSpecificGravity(specificGravityRepository.findById(test.getSpecificGravity().getId()).orElse(null));
+        test.setStorage(storageRepository.findById(test.getStorage().getId()).orElse(null));
+        test.setTemperature(temperatureRepository.findById(test.getTemperature().getId()).orElse(null));
+        test.setTotalBacteriaCount(totalBacteriaCountRepository.findById(test.getTotalBacteriaCount().getId()).orElse(null));
+        test.setViscosity(viscosityRepository.findById(test.getViscosity().getId()).orElse(null));
+
+        Test savedTest = testRepository.save(test);
+        return ResponseEntity.ok(savedTest);
+    }
+
 }

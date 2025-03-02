a4fc85aab7d48b678576276c6d198dc5f69654cd
ValentinaLujan12 <vlujan@unal.edu.co>
2025-03-02 12:20:01 -0500

    Update TechnicianScheduleRepository.java

--- src/main/java/com/prebel/prototipo/webapp/repositories/weekly_planner_repositories/TechnicianScheduleRepository.java
+++ src/main/java/com/prebel/prototipo/webapp/repositories/weekly_planner_repositories/TechnicianScheduleRepository.java
@@ -4,10 +4,11 @@ import com.prebel.prototipo.webapp.models.role_module.User;
 import com.prebel.prototipo.webapp.models.weekly_planner.TechnicianSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
-public interface TechnicianScheduleRepository extends JpaRepository<TechnicianSchedule, Long> {
+public interface TechnicianScheduleRepository extends CrudRepository<TechnicianSchedule, Long> {
     List<TechnicianSchedule> findByTechnician(User technician);
 
     @Query("SELECT ts FROM TechnicianSchedule ts JOIN FETCH ts.technician t WHERE t.id = :technicianId")
